cf838120bb8d4dbf2a3184068e8ab722523f10a3
Jason Priestley <jasonhpriestley@gmail.com>
2023-04-02 18:24:11 -0700

    deepcopy

--- ll-parser.js
+++ ll-parser.js
@@ -1,4 +1,4 @@
-let { cata, I, K, constructors } = require("church-cat");
+let { cata, I, K, constructors, getSchema, tagWithSchema } = require("church-cat");
 
 let nodeSchema = ({
     left: [I],
@@ -11,20 +11,40 @@ let nodeSchema = ({
 
 let { left: LeftN, right: RightN, seq: SeqN, eps: EpsN, tok: TokN, placeholder: PlaceholderN } = constructors( nodeSchema );
 
-let fixGrammarCata = cataF => {
+let deepCopy = function(adt) {
+    let schema = getSchema(adt);
+    let ctors = constructors(schema);
+    return f(adt);
+    function f(adt) {
+        return adt(Object.fromEntries(
+            Object.entries(schema).map(
+                ([key, argTypes]) => [
+                    key, 
+                    (...args) => {
+                        let children = argTypes.map((t,ii) => t({I: () => f(args[ii]), K: () => args[ii]}));
+                        return ctors[ key ](...children);
+                    }
+                ]
+            )
+        ));
+    }
+}
+
+let fixGrammarCata = (cataF, def=() => null, eq=(l,r) => l === r) => {
     let memo = new WeakMap();
     return function(gmr) {
         if(memo.has(gmr)) return memo.get(gmr);
-        let f, done = false;
+        let f, done = false, N = 10;
         while(!done) {
+            if(N-- === 0) throw new Error("Ran out of tries");
             let old = f;
             done = true;
             f = cata({
                 alt: function(l,r) { 
                     return up => {
-                        let lv = l([old ? old(this) : null, up]), rv = r([old ? old(this) : null, up]);
-                        if(old && this({alt: (l,r) => old(l) === lv && old(r) === rv})) {
-                            return old(this);
+                        let lv = l([old ? old(gmr, this) : def(), up]), rv = r([old ? old(gmr, this) : def(), up]);
+                        if(old && this({alt: (l,r) => eq(old(gmr, l), lv) && eq(old(gmr, r), rv)})) {
+                            return old(gmr, this);
                         } else {
                             done = false;
                             return cataF(gmr).alt.call(this, lv,rv);
@@ -33,47 +53,46 @@ let fixGrammarCata = cataF => {
                 },
                 seq: function(l,r) {
                     return up => {
-                        let lv = l([old ? old(this) : null, up]), rv = r([old ? old(this) : null, up]);
-                        if(old && this({seq: (l,r) => old(l) === lv && old(r) === rv})) {
-                            return old(this);
+                        let lv = l([old ? old(gmr, this) : def(), up]), rv = r([old ? old(gmr, this) : def(), up]);
+                        if(old && this({seq: (l,r) => eq(old(gmr, l), lv) && eq(old(gmr, r), rv)})) {
+                            return old(gmr, this);
                         } else {
                             done = false;
                             return cataF(gmr).seq.call(this, lv,rv);
                         }
                     }
                 },
-                tok: function(t) { return up => old ? old(this) : cataF(gmr).tok.call(this, t) },
+                tok: function(t) { return up => old ? old(gmr, this) : cataF(gmr).tok.call(this, t) },
                 jump: (n) => up => { let lst = up; while(--n > 0) lst = lst[1]; return lst[0]; },
-                eps: function() { return up => old ? old(this) : cataF(gmr).eps.call(this) }
+                eps: function() { return up => old ? old(gmr, this) : cataF(gmr).eps.call(this) }
             }, []);
             f(gmr, gmr);
         }
-        memo.set(gmr, f);
-        return (g) => f(gmr, g);
+        memo.set(gmr, (g) => f(gmr, g));
+        return memo.get(gmr);
     };
 }
-    
 
-let emptyNode = fixGrammarCata(gmr => {
+let emptyNode = fixGrammarCata(gmr => ({
     tok: () => null,
     eps: () => EpsN(),
     alt: (l,r) => l && LeftN(l) || r && RightN(r),
     seq: (l,r) => l && r && SeqN(l,r)
-});
+}));
 
-let emptyOrPlaceholder = fixGrammarCata(gmr => {
-    tok: () => PlaceholderN(),
+let emptyOrPlaceholder = fixGrammarCata(gmr => ({
+    tok: (t) => t === '(' || t === ',' || t === ')' ? TokN(t, t) : PlaceholderN(),
     eps: () => EpsN(),
     seq: (l,r) => SeqN(l,r),
     alt: function(l,r) { return this({alt: (l,r) => emptyNode(gmr)(l) || emptyNode(gmr)(r) || PlaceholderN()}); }
-});
+}), () => PlaceholderN());
 
-let firstSet = fixGrammarCata(gmr => {
+let firstSet = fixGrammarCata(gmr => ({
     eps: () => new Set(),
     tok: (t) => new Set([t]),
     seq: function(l,r) { return this({seq: (lo,ro) => emptyNode(gmr)(lo) ? new Set([...l, ...r]) : l}); },
     alt: (l,r) => new Set([...l, ...r])
-});
+}), () => new Set());
 
 let resolveJumps = (gs) => gs[0]({
     jump: (n) => { let lst = gs; while(n-- > 0) lst = lst[1]; return lst; },
@@ -140,7 +159,7 @@ let llParser = (gmr) => {
                         } 
                     }
                     if(emptyNode(gmr)(this)) {
-                        return [emptyNode(gmr)(this), input];
+                        return [deepCopy(emptyNode(gmr)(this)), input];
                     } else {
                         throw new Error("Parse failed: end of input");
                     }
@@ -148,14 +167,15 @@ let llParser = (gmr) => {
                 return fn;
             }
         }
-    }, [])(gmr);
+    }, [])(gmr, gmr);
 };
 
-function extractValue(gmr, adt, fns, ftoken, fplaceholder) {
-    nodeGrammar(gmr, adt);
+function extractValue(gmr, rootAdt, fns, fplaceholder) {
     function runFns(adt, args) {
-        let g = nodeGrammar(gmr, adt);
-        if(fns.has(g)) return [fns.get(g)(args)];
+        let g = nodeGrammar(gmr, rootAdt, adt);
+        if(fns.has(g)) {
+            return [fns.get(g)(args)];
+        }
         return args;
     }
     return cata({
@@ -164,8 +184,8 @@ function extractValue(gmr, adt, fns, ftoken, fplaceholder) {
         seq: function(l,r) { return runFns(this, [...l, ...r]) },
         tok: function(t, v) { return runFns(this, [this]) },
         eps: function() { return runFns(this, []) },
-        placeholder: function() { return fplaceholder() }
-    })(adt, adt)[0];
+        placeholder: function() { return runFns(this, [fplaceholder(this)]) }
+    })(rootAdt, rootAdt)[0];
 }
 
-module.exports = {llParser, extractValue, LeftN, RightN, SeqN, EpsN, TokN, PlaceholderN, nodeSchema, emptyNode, emptyOrPlaceholder, nodeGrammar };
+module.exports = {deepCopy, llParser, extractValue, LeftN, RightN, SeqN, EpsN, TokN, PlaceholderN, nodeSchema, emptyNode, emptyOrPlaceholder, nodeGrammar, fixGrammarCata };
